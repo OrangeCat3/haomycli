924269699ccef1af3f651240ce732fd07a0920f3
DESKTOP-JDMIBKF\15806 <1580660181@qq.com>
2021-03-30 00:39:48 +0800

    第一次提交

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+
+//查看版本号
+program.version(require('./package.json').version,'-v,--version')
+
+//增加自己的option
+//也是--help的提示
+program.option('-r -why')
+
+program.parse(process.argv)
