84b854647bba4dfdf35ee62761d7c1b4ef733374
DESKTOP-JDMIBKF\15806 <1580660181@qq.com>
2021-03-31 01:14:25 +0800

    11111111111111

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ program.version(require('./package.json').version,'-v,--version')
 
 //增加自己的option
 //也是--help的提示
-program.option('-r -why')
+program.option('-r -why',"a why cli")
 
 program.parse(process.argv)
